4ad676caa50e7d479ee1ce01a1fc42cf0c3f3fb8
menghh131620 <51124448+menghh131620@users.noreply.github.com>
2019-05-30 17:29:40 +0800

    Update demoA.py

--- demoA.py
+++ demoA.py
@@ -1,5 +1,16 @@
-import xlwt
-new_workbook=xlwt.workbook()
-worksheet=new_workbook.add_sheet('new_test')
-worksheet.write(0,0,'test')
-new_workbook('d:test.xls')
+import requests as req
+import json
+
+apiURL='{url}?q={city}&APPID={key}'.format(
+url = 'http://api.openweathermap.org/data/2.5/weather',
+city = 'Babu,CN',
+key = 'bf1b626f3ed6c480088d28d9a053e358'
+)
+
+r = req.get(apiURL)
+obj = json.loads(r.text)
+
+
+city = obj['name']
+icon = obj['weather'][0]['icon']
+humid = obj['main']['humidity']
