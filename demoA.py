9e24d03dfa3fa9e8d091681d2d0a2c8eceb1a9f4
menghh131620 <51124448+menghh131620@users.noreply.github.com>
2019-05-30 17:13:13 +0800

    Update demoA.py

--- demoA.py
+++ demoA.py
@@ -1,16 +1,37 @@
+import urllib.request
 import requests as req
 import json
+class WeatherData:
+    
+    temperature = ''
+    humid=''
+    key = 'bf1b626f3ed6c480088d28d9a053e358'
+   
+    def __init__(self, city):
+        self.city = city
+        apiURL='{url}?q={city}&APPID={key}'.format(
+        url = 'http://api.openweathermap.org/data/2.5/weather',
+        city = self.city,
+        key = self.key
+        )
 
-apiURL='{url}?q={city}&APPID={key}'.format(
-url = 'http://api.openweathermap.org/data/2.5/weather',
-city = 'Babu,CN',
-key = 'bf1b626f3ed6c480088d28d9a053e358'
-)
-
-r = req.get(apiURL)
-obj = json.loads(r.text)
+        r = req.get(apiURL)
+        obj = json.loads(r.text)
 
 
-city = obj['name']
-icon = obj['weather'][0]['icon']
-humid = obj['main']['humidity']
+        
+        self.icon = obj['weather'][0]['icon']
+        self.humid = obj['main']['humidity']
+    
+    
+    def getTemperature(self):
+        return self.temperature
+    def getHumid(self):
+        return self.humid
+        
+    
+    
+if __name__ == "__main__":
+    
+    current_weather = WeatherData('Babu,CN')
+    print(current_weather.getHumid())
